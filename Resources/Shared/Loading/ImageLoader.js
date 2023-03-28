a8b630d40561a4f0e476bd6c247dbe1894eb4ba1
RyanTheTechMan <ryan@rivergroup.com>
2023-03-27 22:56:19 -0400

    Cleaned up file

--- Resources/Shared/Loading/ImageLoader.js
+++ Resources/Shared/Loading/ImageLoader.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import {Image, StyleSheet} from 'react-native';
-import {SvgUri} from 'react-native-svg';
+import { Image, StyleSheet } from 'react-native';
+import { SvgUri } from 'react-native-svg';
 
+// Helper function to check if the source is an SVG
 const isSvgSource = (src) => {
   return typeof src === 'string' && src.toLowerCase().endsWith('.svg');
 };
@@ -9,21 +10,35 @@ const isSvgSource = (src) => {
 const ImageLoader = ({ source, loadingSource, errorSource, style, ...props }) => {
   const [currentSource, setCurrentSource] = useState(loadingSource);
 
+  // Prefetch the image and update the source accordingly
   useEffect(() => {
     if (source) {
-      const uri = isSvgSource(source) ? source : typeof source === 'number' ? Image.resolveAssetSource(source).uri : source;
+      const uri = isSvgSource(source)
+        ? source
+        : typeof source === 'number'
+          ? Image.resolveAssetSource(source).uri
+          : source;
       Image.prefetch(uri)
         .then(() => setCurrentSource(source))
         .catch(() => setCurrentSource(errorSource));
     }
   }, [source, errorSource]);
 
+  // Render the appropriate image component based on the source type
   const renderImage = () => {
     if (typeof currentSource === 'string') {
       if (isSvgSource(currentSource)) {
-        return <SvgUri uri={currentSource} style={[styles.image, style]} {...props} />;
+        return (
+          <SvgUri uri={currentSource} style={[styles.image, style]} {...props} />
+        );
       } else {
-        return <Image source={{ uri: currentSource }} style={[styles.image, style]} {...props} />;
+        return (
+          <Image
+            source={{ uri: currentSource }}
+            style={[styles.image, style]}
+            {...props}
+          />
+        );
       }
     } else if (React.isValidElement(currentSource)) {
       return React.cloneElement(currentSource, {
@@ -31,7 +46,9 @@ const ImageLoader = ({ source, loadingSource, errorSource, style, ...props }) =>
         style: [styles.image, style],
       });
     } else {
-      return <Image source={currentSource} style={[styles.image, style]} {...props} />;
+      return (
+        <Image source={currentSource} style={[styles.image, style]} {...props} />
+      );
     }
   };
 
