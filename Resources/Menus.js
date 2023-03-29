dd1122d8722e79611b5dc1cb9ad4fa957c53805b
HamSandwichWithHam <johann@roanoke.edu>
2023-03-29 11:51:43 -0400

    Added cavernMenu
    Not updating with additional changes to cavernMenu object

--- Resources/Menus.js
+++ Resources/Menus.js
@@ -1,81 +1,129 @@
 import { StyleSheet, Text, View, SectionList, FlatList, ScrollView } from 'react-native';
 
 export const commonsMenu = [
-    {
-        day: 'Sunday',
-        meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
-    },
-    {
-        day: 'Monday',
-        meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
-    },    {
-        day: 'Tuesday',
-        meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
-    },    {
-        day: 'Wednesday',
-        meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
-    },    {
-        day: 'Thursday',
-        meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
-    },    {
-        day: 'Friday',
-        meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
-    },    {
-        day: 'Saturday',
-        meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
-    },
+  {
+    day: 'Sunday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  },
+  {
+    day: 'Monday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Tuesday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Wednesday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Thursday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Friday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Saturday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  },
 ]
 
-export function DiningPage({route, navigation}) {
-  const {data} = route.params;
-    console.log(data);
-    return (
-      <View style={styles.page}>
-        <FlatList
-          data={data}
-          renderItem={({ item }) =>
-            <View>
-              <Text style={styles.day}> {item.day}</Text>
-              <FlatList
-                data={item.meals}
-                renderItem={({ item }) =>
-                  <View>
-                    <Text style={styles.meal}>{item.meal}</Text>
-                    <FlatList
-                      data={item.mealItems}
-                      renderItem={({ item }) =>
-                        <Text>{item}</Text>
-                      }
-                    />
-  
-                  </View>
-                }
-  
-              />
-            </View>
-          }
-        />
-      </View>
-    );
-  }
+export const freshensMenu = [
+  {
+    day: 'Sunday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  },
+  {
+    day: 'Monday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Tuesday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Wednesday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Thursday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Friday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  }, {
+    day: 'Saturday',
+    meals: [{ meal: "Breakfast", mealItems: ["Eggs", "Toast"] }, { meal: "Lunch", mealItems: ["Sandwiches", "Soup", "Cookies"] }, { meal: "Dinner", mealItems: ["Pizza", "Salad", "Burgers"] }]
+  },
+]
 
-  export const styles = StyleSheet.create({
-    page: {
-      flex: 1,
-      alignItems: 'center',
-      fontColor: 'black',
-    },
-    day: {
-      fontSize: 40,
-      alignItems: 'left',
-      marginTop: 20,
-    },
-    meal: {
-      flex: 1,
-      fontSize: 20,
-      marginTop: 20,
-      alignItems: 'center',
-  
-    }
-  });
-  
+export const cavernMenu = [
+  {
+    day: 'Monday',
+    meals: [{ meal: "Dinner", mealItems: ["Fries", "Salad", "Burgers", "Chicken Sandwich"] }]
+  }, {
+    day: 'Tuesday',
+    meals: [{ meal: "Dinner", mealItems: ["Fries", "Salad", "Burgers", "Chicken Sandwich"] }]
+  }, {
+    day: 'Wednesday',
+    meals: [{ meal: "Dinner", mealItems: ["Fries", "Salad", "Burgers", "Chicken Sandwich"] }]
+  }, {
+    day: 'Thursday',
+    meals: [{ meal: "Dinner", mealItems: ["Fries", "Salad", "Burgers", "Chicken Sandwich"] }]
+  }, {
+    day: 'Friday',
+    meals: [{ meal: "Dinner", mealItems: ["Fries", "Salad", "Burgers", "Chicken Sandwich"] }]
+  }, {
+    day: 'Saturday',
+    meals: [{ meal: "Dinner", mealItems: ["Fries", "Salad", "Burgers", "Chicken Sandwich"] }]
+  },
+
+]
+
+export function DiningPage({ route, navigation }) {
+  const { data } = route.params;
+  console.log(data);
+  return (
+    <View style={styles.page}>
+      <FlatList
+        data={data}
+        renderItem={({ item }) =>
+          <View>
+            <Text style={styles.day}> {item.day}</Text>
+            <FlatList
+              data={item.meals}
+              renderItem={({ item }) =>
+                <View>
+                  <Text style={styles.meal}>{item.meal}</Text>
+                  <FlatList
+                    data={item.mealItems}
+                    renderItem={({ item }) =>
+                      <Text>{item}</Text>
+                    }
+                  />
+
+                </View>
+              }
+
+            />
+          </View>
+        }
+      />
+    </View>
+  );
+}
+
+export const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    alignItems: 'center',
+    fontColor: 'black',
+  },
+  day: {
+    fontSize: 40,
+    alignItems: 'left',
+    marginTop: 20,
+  },
+  meal: {
+    flex: 1,
+    fontSize: 20,
+    marginTop: 20,
+    alignItems: 'center',
+
+  }
+});
