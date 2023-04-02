4c0a3fd901598f34b1e7574527fac650e044a7bb
HamSandwichWithHam <johann@roanoke.edu>
2023-04-02 16:11:03 -0400

    Mail update
    Prints from student info object

--- Resources/Pages/Mail.js
+++ Resources/Pages/Mail.js
@@ -1,10 +1,62 @@
-import { StyleSheet, Text, View } from "react-native";
+import { setStatusBarHidden } from "expo-status-bar";
+import { StyleSheet, Text, View, } from "react-native";
+import React, { useState } from 'react';
+
+
+
+const studentInfoGroup = [
+
+    {
+        studentId: '0906523',
+        studentName: {
+            firstName: 'Sarah',
+            lastName: 'Connor',
+        },
+        collegeAddress: {
+            street: '221 College Lane',
+            campusBox: '42',
+            city: 'Salem',
+            state: 'VA',
+            zipCode: '24153',
+            campusBoxCode: '4572'
+        }
+    },
+    {
+        studentId: '0813342',
+        studentName: {
+            firstName: 'Joseph',
+            lastName: 'Student',
+        },
+        collegeAddress: {
+            street: '221 College Lane',
+            campusBox: '132',
+            city: 'Salem',
+            state: 'VA',
+            zipCode: '24153',
+            campusBoxCode: '3923'
+        }
+    },
+];
+
 
 export default function MailPage() {
 
+    const [selectedId, setSelectedId] = useState('0813342');
+    const [currentStudent, setCurrentStudent] = useState(studentInfoGroup[0]);
+
+    for (student in studentInfoGroup) {
+        if (student.studentId === selectedId) {
+            setCurrentStudent(student);
+            return;
+        }
+    }
+
     return (
         <View style={styles.view}>
-            <Text style={styles.test}>Mail Page</Text>
+            <Text>Campus Address:</Text>
+            <Text>{currentStudent.studentName.firstName}</Text>
+            <Text></Text>
+            <Text></Text>
         </View>
     );
 
